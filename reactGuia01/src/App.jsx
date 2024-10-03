ddf7119881866749f7f66e7e0f46891f12f717cd
Elinn-16 <martinaenievas@escuelasproa.edu.ar>
2024-10-03 13:25:38 -0300

    Falta manejo de errores

--- reactGuia01/src/App.jsx
+++ reactGuia01/src/App.jsx
@@ -1,35 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputName, setInputName] = useState('');
+  const [inputEmail, setInputEmail] = useState('');
+  const [contactoGuardado, setContactoGuardado] = useState(null); 
+
+  const guardarNombre = (event) => {
+    setInputName(event.target.value);
+    console.log("Nombre:", event.target.value);
+  };
+
+  const guardarEmail = (event) => {
+    setInputEmail(event.target.value);
+    console.log("Email:", event.target.value);
+  };
+
+  const manejarGuardarContacto = () => { 
+    setContactoGuardado(`Nombre: ${inputName}, Email: ${inputEmail}`);
+  };
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Agenda de contactos</h1>
+      <input type="text" placeholder="Nombre" onChange={guardarNombre} />
+      <input type="email" placeholder="Email" onChange={guardarEmail} />
+      <button onClick={manejarGuardarContacto}>Guardar Contacto</button>
+      <p>{contactoGuardado}</p>
+    </div>
+  );
 }
+
 
 export default App
